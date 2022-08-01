68ae5e858f6a0496b8da080c1826aa21b667a6cf
Amir Dekel <6893125+adql@users.noreply.github.com>
2022-08-01 11:05:47 +0200

    Tick rate controller

--- src/Gol.purs
+++ src/Gol.purs
@@ -3,6 +3,7 @@ module Gol where
 import Prelude
 
 import Data.Array2D (replicate2D)
+import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
 import Data.Nullable (null)
 import Data.Traversable (sequence)
@@ -13,10 +14,10 @@ import Effect.Timer (clearInterval, setInterval)
 import Gol.Logic (World, tick)
 import Gol.Render (mkWorldGrid, renderWorld)
 import React.Basic.DOM as D
-import React.Basic.DOM.Events (capture_)
+import React.Basic.DOM.Events (capture, capture_, targetValue)
 import React.Basic.Hooks (Component, component, readRefMaybe, useRef, useState)
 import React.Basic.Hooks as React
-import Utils (nodeToCanvasElement)
+import Utils (nodeToCanvasElement, toInterval)
 
 type CanvasSize = { width::String, height::String }
 
@@ -31,6 +32,7 @@ mkGol world0 size = do
   component "Gol" \_ -> React.do
     world /\ setWorld <- useState world0
     running /\ setRunning <- useState true
+    fr /\ setFr <- useState 5
     canvas <- useRef null
   
     React.useEffect world do
@@ -42,9 +44,9 @@ mkGol world0 size = do
           renderWorld grid world
           pure mempty
 
-    React.useEffect running $
+    React.useEffect { running, fr } $
       if running then do
-        intervalId <- setInterval 50 $ setWorld (\w -> tick w)
+        intervalId <- setInterval (toInterval fr) $ setWorld (\w -> tick w)
         pure $ clearInterval intervalId
       else pure mempty
   
@@ -58,5 +60,15 @@ mkGol world0 size = do
                          }
               , D.button { onClick: capture_ $ setRunning $ \r -> not r
                          , children: [ D.text $ if running then "Stop" else "Start" ] }
+              , D.input { type:"range"
+                        , min:"1"
+                        , max:"100"
+                        , defaultValue:show fr
+                        , onChange: capture targetValue $ \v -> case map fromString v of
+                            Nothing -> pure unit
+                            Just Nothing -> pure unit
+                            Just (Just rate) -> setFr ( \_ -> rate ) *>
+                                                setRunning \r -> not $ not r
+                        }
               ]
             }
--- src/Utils.purs
+++ src/Utils.purs
@@ -1,8 +1,13 @@
 module Utils where
 
+import Prelude
+
 import Graphics.Canvas (CanvasElement)
 import Unsafe.Coerce (unsafeCoerce)
 import Web.DOM (Node)
 
 nodeToCanvasElement :: Node -> CanvasElement
 nodeToCanvasElement = unsafeCoerce
+
+toInterval :: Int -> Int
+toInterval fr = 1000 / fr
